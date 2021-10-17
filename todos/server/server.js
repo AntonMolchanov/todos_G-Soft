f98cad5e24ff1c62ba088f62aca0660153b90402
AntonMolchanov <antonmolchanov97@gmail.com>
2021-10-17 19:22:57 +0300

    applied changes to paths in server.js

--- todos/server/server.js
+++ todos/server/server.js
@@ -5,10 +5,10 @@ require ('dotenv').config()
 console.log(process.env)
 const port = process.env.PORT || 8080
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port,() => {
