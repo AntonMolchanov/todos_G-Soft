56bd37845c5a0526f901ec84e2ee55cb4755c7b5
AntonMolchanov <antonmolchanov97@gmail.com>
2021-10-17 20:51:43 +0300

    changed the package.json file

--- todos/server/server.js
+++ todos/server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 require ('dotenv').config()
-// console.log(process.env)
 const port = process.env.PORT || 9000
 
 app.use(express.static(path.join(__dirname, '../build')));
